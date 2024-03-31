cf45fa429d80effbb68adedec667b4fee3f60e2d
Justin B. Yee <jbyee2015@gmail.com>
2024-03-31 10:10:38 -0700

    Added endpoint for liking/unliking posts.

--- siteapps/socialmedia/urls.py
+++ siteapps/socialmedia/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from siteapps.socialmedia.views import CreatePostView, GetRecentPostsView
+from siteapps.socialmedia.views import CreatePostView, GetRecentPostsView, LikePostView
 
 urlpatterns = [
-    path("api/post/create/", CreatePostView.as_view(), name="create_post"),
-    path("api/feed/get_recent_posts/", GetRecentPostsView.as_view(), name="get_recent_posts"),
+    path("api/posts/create/", CreatePostView.as_view(), name="create_post"),
+    path("api/posts/like/", LikePostView.as_view(), name="like_post"),
+    path("api/feed/get/", GetRecentPostsView.as_view(), name="get_posts"),
 ]
