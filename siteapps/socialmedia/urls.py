68ff45c404af3d9e4a2d93a621509b7f5c5e8268
Justin B. Yee <jbyee2015@gmail.com>
2024-04-01 22:01:38 -0700

    Added separate authenticated/no auth endpoints for getting posts, so user info can be tracked.

--- siteapps/socialmedia/urls.py
+++ siteapps/socialmedia/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from siteapps.socialmedia.views import CreatePostView, GetRecentPostsView, LikePostView
+from siteapps.socialmedia.views import (
+    CreatePostView,
+    GetRecentPostsAuthenticatedView,
+    GetRecentPostsNoAuthView,
+    LikePostView,
+)
 
 urlpatterns = [
     path("api/posts/create/", CreatePostView.as_view(), name="create_post"),
     path("api/posts/like/", LikePostView.as_view(), name="like_post"),
-    path("api/feed/get/", GetRecentPostsView.as_view(), name="get_posts"),
+    path("api/feed/get/auth", GetRecentPostsAuthenticatedView.as_view(), name="get_posts_authenticated"),
+    path("api/feed/get/noauth", GetRecentPostsNoAuthView.as_view(), name="get_posts_no_auth"),
 ]
