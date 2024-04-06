882e3ec2b9560f3ae12ba5cf0c9d5e3e05c2bb60
Justin B. Yee <jbyee2015@gmail.com>
2024-04-06 10:10:58 -0700

    Added endpoint to retrieve user profile information.

--- siteapps/users/urls.py
+++ siteapps/users/urls.py
@@ -4,9 +4,11 @@ from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path
 
 from . import views
+from .views import UserProfileView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
+    path("profile/", UserProfileView.as_view(), name="user_profile"),
 ]
--- siteapps/users/views.py
+++ siteapps/users/views.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.shortcuts import render
+from rest_framework import authentication, permissions, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from siteapps.socialmedia.models import MediaPost
+
 
 # Create your views here.
+class UserProfileView(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        data = {
+            "joined_date": request.user.created.strftime(settings.READABLE_DATE_FORMAT),
+            "display_name": request.user.name,
+            "sightings_count": MediaPost.objects.filter(created_by=request.user).count(),
+        }
+
+        return Response(status=status.HTTP_200_OK, data=data)
