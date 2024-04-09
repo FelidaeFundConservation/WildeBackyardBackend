982b444c9ebe0609936c72fab9946aad4369b53e
Justin B. Yee <jbyee2015@gmail.com>
2024-04-09 11:46:22 -0700

    Removed console email backend in prod settings.

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -43,8 +43,6 @@ AZURE_STORAGE_CONTAINER_NAME = env.str(
 
 # EMAIL
 # ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-timeout
 EMAIL_TIMEOUT = 5
 
