4d8e54995d641f10a52fc3744e21ee5e51e613b4
Justin B. Yee <jbyee2015@gmail.com>
2024-06-28 17:16:46 -0700

    Overrode get_cache_key() in Mapbox API throttle classes to fix error.

--- siteapps/mapbox/throttles.py
+++ siteapps/mapbox/throttles.py
@@ -5,17 +5,29 @@ class SearchSuggestionsPerMinuteThrottle(SimpleRateThrottle):
     scope = "search_suggestions_per_minute"
     rate = "50/min"
 
+    def get_cache_key(self, key, *args, **kwargs):
+        return f"perMinuteSearchSuggestions{hash(key)}"
+
 
 class GeocodePerMinuteThrottle(SimpleRateThrottle):
     scope = "geocode_per_minute"
     rate = "5/min"
+
+    def get_cache_key(self, key, *args, **kwargs):
+        return f"perMinuteGeocoding{hash(key)}"
 
 
 class SearchSuggestionsPerDayThrottle(SimpleRateThrottle):
     scope = "search_suggestions_per_day"
     rate = "250/day"
 
+    def get_cache_key(self, key, *args, **kwargs):
+        return f"dailySearchSuggestions{hash(key)}"
+
 
 class GeocodePerDayThrottle(SimpleRateThrottle):
     scope = "geocode_per_day"
     rate = "50/day"
+
+    def get_cache_key(self, key, *args, **kwargs):
+        return f"dailyGeocoding{hash(key)}"
