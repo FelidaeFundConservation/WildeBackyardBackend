fbcf687c2e989e46ab0fa64120a6211f07209786
Justin B. Yee <jbyee2015@gmail.com>
2024-07-01 09:35:15 -0700

    Added 'resend confirmation email' endpoint for account registration.

--- siteapps/users/urls.py
+++ siteapps/users/urls.py
@@ -1,13 +1,13 @@
 from allauth.account.views import PasswordChangeView
-from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.registration.views import RegisterView, ResendEmailVerificationView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path
 
-from . import views
 from .views import ChangeUsernameView, DeleteAccountView, UserProfileView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
+    path("resend_verification_email/", ResendEmailVerificationView.as_view(), name="resend_email_confirmation"),
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
     path("profile/", UserProfileView.as_view(), name="user_profile"),
