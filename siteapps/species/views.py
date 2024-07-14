da256de5f1d5f89068226d315ca6c51a5174bb6a
Justin B. Yee <jbyee2015@gmail.com>
2024-07-14 09:01:06 -0700

    Added endpoint to create new species names.

--- siteapps/species/views.py
+++ siteapps/species/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from rest_framework import authentication, permissions, status
 from rest_framework.response import Response
@@ -11,3 +13,25 @@ class GetSpeciesNamesView(APIView):
     def get(self, request):
         species_names = list(SpeciesName.objects.all().values_list("name", flat=True))
         return Response(status=status.HTTP_200_OK, data=species_names)
+
+
+class CreateSpeciesNameView(APIView):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        species_name = data.get("name")
+
+        if species_name is None:
+            message = "Species name was not provided."
+
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"error": message},
+            )
+        else:
+            # Names should be in title case to avoid duplications
+            species_name = species_name.title()
+            # Create a new species with name if it doesn't exist
+            SpeciesName.objects.get_or_create(name=species_name)
+
+        return Response(status=status.HTTP_201_CREATED)
