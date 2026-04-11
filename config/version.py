4b578c1fe2dafba863a192693c380560ef55adc9
oldtopos <oldtopos@edgeoftheblade.com>
2026-04-10 19:44:57 -0700

    Fix deploy script to exclude version.py from uncommitted changes check

--- config/version.py
+++ config/version.py
@@ -4,7 +4,7 @@ This file is automatically updated by the deployment script.
 """
 
 VERSION = {
-    "commit_hash": "5c2a08d",
+    "commit_hash": "2d862b4",
     "release_tag": "dev",
     "branch": "main",
 }
